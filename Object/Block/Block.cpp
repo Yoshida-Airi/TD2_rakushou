834e844f5a03c939ca4224acac00f4c294c36ffe
k022g0004@g.neec.ac.jp <k022g0004@g.neec.ac.jp>
2023-10-23 16:10:47 +0900

    ブロックの追加

---
+++ Object/Block/Block.cpp
@@ -0,0 +1,46 @@
+#include "Block.h"
+#include "TextureManager.h"
+#include "Player.h"
+#include "ImGuiManager.h"
+
+void Block::Initialize(Model* model, Vector3 translation) {
+	// NULLポインタチェック
+	assert(model);
+	model_ = model;
+
+
+	worldTransform_.Initialize();
+
+	worldTransform_.translation_ = translation;
+}
+
+Block::~Block() {}
+
+void Block::Move() {
+	worldTransform_.translation_.x -= velocity_.x;
+	worldTransform_.translation_.y -= velocity_.y;
+	worldTransform_.translation_.z -= velocity_.z;
+}
+
+void Block::Update() {
+	// ワールドトランスフォームの更新
+	worldTransform_.UpdateMatrix();
+}
+
+void Block::Draw(const ViewProjection& viewProjection) {
+	// モデル描画
+	model_->Draw(viewProjection,worldTransform_);
+}
+
+void Block::OnCollision() {}
+
+Vector3 Block::GetWorldPosition() {
+	// ワールド座標を入れる変数
+	Vector3 worldPos;
+	// ワールド行列の平行移動成分を取得
+	worldPos.x = worldTransform_.matWorld_.m[3][0];
+	worldPos.y = worldTransform_.matWorld_.m[3][1];
+	worldPos.z = worldTransform_.matWorld_.m[3][2];
+
+	return worldPos;
+}
---
+++ Object/Block/Block.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Input.h"
+#include "Model.h"
+#include "WorldTransform.h"
+#include <cassert>
+
+class Block {
+public:
+	void Initialize(Model* model, Vector3 translation);
+	~Block();
+
+	void Move();
+
+	void Update();
+
+	void Draw(const ViewProjection& viewProjection);
+
+	void OnCollision();
+
+	void SetType(float type) { type_ = type; }
+
+	// ワールド座標を取得
+	Vector3 GetWorldPosition();
+	Vector3 GetWorldTransform() { return worldTransform_.translation_; }
+	float GetType() { return type_; }
+
+private:
+	// ワールド変換データ0
+	WorldTransform worldTransform_;
+	// モデル
+	Model* model_ = nullptr;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	// 速度
+	Vector3 velocity_;
+	// ブロックのタイプ
+	// 下が0、上が1、左右が2
+	float type_ = 0;
+};
